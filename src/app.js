82b96b5ae453a0d9363b6a47e5510fe42d12b4be
Benito Serrano <benosg@gmail.com>
2023-08-20 20:41:50 -0400

    owasp

--- src/app.js
+++ src/app.js
@@ -2,14 +2,15 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const authRoutes = require('./routes/authRoutes');
 const userRoutes = require('./routes/userRoutes');
-
+const helmet = require('helmet');
 const app = express();
-
+app.disable('x-powered-by');
+app.use(helmet());
 app.use(bodyParser.json());
 
 app.use('/auth', authRoutes);
 app.use('/user', userRoutes);
-// Definir la ruta para la verificación de correo electrónico
+
 app.listen(3000, () => {
-  console.log('Servidor iniciado en el puerto 3000.');
+  
 });
