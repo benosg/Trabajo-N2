50927b393ba6a974aed48a17ab42cadf955beb96
Rodrigo Pantoja <rodrigo.pantoja@uc.cl>
2023-08-23 09:05:02 -0400

    1. Creación de conexión a api de prueba 2. Pruebas de conexión y entrega de datos a api de prueba 3. Manejo de errores y entrega exitosa.

--- Front-end/src/app/services/global.ts
+++ Front-end/src/app/services/global.ts
@@ -2,6 +2,6 @@
 export var global = {
     api_post: "https://newsapi.org/v2/everything?sources=techcrunch&apiKey=1ac702adc5f0483e9765a9b9d0e968f5",
     api_post_jsonserver: "http://localhost:3000/posts", /* ?_page=1&_limit=8 */
-    api_news_jsonserver: "http://localhost:3000/posts"
+    api_news_jsonserver: "http://localhost:3000/news"
 };
 
--- Front-end/src/app/template/suscripcion/suscripcion.component.ts
+++ Front-end/src/app/template/suscripcion/suscripcion.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { QueriesService }    from '../../services/queries.service';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { global }            from '../../services/global';
@@ -10,14 +10,13 @@ import { Suscribir }         from '../../services/suscribir';
   styleUrls: ['./suscripcion.component.css']
 })
 export class SuscripcionComponent implements OnInit {
-
+  
   private api_news_jsonserver : string  = global.api_news_jsonserver;
-  private respuesta_api       : string;
-  private response_api        : any     = {
+  private suscribirModel                = new Suscribir("","");
+  public response_api_news    : any     = {
     'show': false,
-    'data': ''
+    'text': ''
   }
-  private suscribirModel = new Suscribir("","");
 
   constructor(
     private queriesService: QueriesService
@@ -26,84 +25,21 @@ export class SuscripcionComponent implements OnInit {
   ngOnInit() {
   }
 
-  /* suscripcion(form){
-    const formData = new FormData();
-    formData.append('array_datos', JSON.stringify(form));
-
-    console.log(form);
-
-    this.http.post(this.api_news_jsonserver, formData)
-      .subscribe(response => {
-
-        console.log(response)
-        if(response['httpCode'] == "200"){
-          this.respuesta_api = "success";
-          form.reset();
-          console.log(response);
-          
-        }else if(response['httpCode']== "403"){
-          this.respuesta_api = "false";
-          console.log("se conectó pero login es incorrecto");
-          console.log(response['resultado'])
-          
-        }else{
-          this.respuesta_api = "error";
-          form.reset();
-          console.log("se conectó pero no trajo resultado 200");
-          console.log(response)
-        }
-      })
-  } */
-
-  /* submitSuscripcion(form, $event) {
-    $event.preventDefault();
-
-    if(form.form.value)
-      this.response.show = true;
-      this.queriesService.queryGet(this.urlPhp+'/mail-contacto/envio.php?nombreApellido='+this.name+'&mail='+this.email+'&consulta='+this.message).then(
-      (data) => {
-        if(data['respuesta'] == true){
-          this.response.show = true;
-          this.response.text = 'Su mensaje ha sido enviado con éxito';
-          this.response.icono = 'check_circle_outline';
-          this.response.color = 'texto-dg';
-          this.name = "";
-          this.email = "";
-          this.message = "";
-        }else{
-          //console.log(data);
-          this.response.show = true;
-          this.response.text = 'Ha ocurrido un problema. Por favor, intente más tarde';
-          this.response.icono = 'highlight_off';
-          this.response.color = 'texto-dg-rojo';
-        }
-      },
-      (error) => {
-        console.log(error);
-        this.response.show = true;
-        this.response.text = 'Ha ocurrido un problema. Por favor, intente más tarde';
-        this.response.icono = 'highlight_off';
-        this.response.color = 'texto-dg-rojo';
-      }
-    );
-  } */
-
-  submitSuscripcion(form, $event) {
-    $event.preventDefault();
-    console.log(this.suscribirModel);
-
-    /* if(form.form.value){
-      this.queriesService.queryPost(this.api_news_jsonserver, form.form.value).then(
+  submitSuscripcion(form) {
+    if(this.suscribirModel.email){
+      this.queriesService.queryPost(this.api_news_jsonserver, this.suscribirModel).then(
         (data) => {
-          this.response_api.show = true;
-          this.response_api.text = 'Su mensaje ha sido enviado con éxito';
+          this.response_api_news.show = true;
+          this.response_api_news.text = 'Su suscripción ha sido enviado con éxito';
+          form.reset()
         },
         (error) => {
-          this.response_api.show = true;
-          this.response_api.text = 'Ha ocurrido un problema. Por favor, intente más tarde';
+          console.log(error); console.log("error")
+          this.response_api_news.show = true;
+          this.response_api_news.text = 'Ha ocurrido un problema. Por favor, intente más tarde';
         }
       );
-    } */
+    }
   }
 
 }
